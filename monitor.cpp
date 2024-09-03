7724aaacdd4c977bfd373c8366b2d4c092d8267b
S. Sasikala <S.Sasikala@philips.com>
2024-09-03 13:22:03 +0530

    Changes for Reduce Complexity

--- monitor.cpp
+++ monitor.cpp
@@ -4,35 +4,66 @@
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
-
+/// @brief displayWarning function to display warning message
+void displayWarning() {
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
+/// @brief to check if the vitals are ok
+/// @param temperature 
+/// @param pulseRate 
+/// @param spo2 
+/// @return 1 if vitals are ok, 0 otherwise
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  if (!checkTemperature(temperature)) {
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  } else if (!checkPulseRate(pulseRate)) {
     return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  } else if (!checkSpo2(spo2)) {
     return 0;
   }
   return 1;
+}
+/// @brief to check if the temperature is in range
+/// @param temperature 
+/// @return true if temperature is in range, false otherwise
+bool checkTemperature(float temperature) {
+  if (temperature > 102 || temperature < 95) {
+    cout << "Temperature is critical!\n";
+    displayWarning();
+    return false;
+  }
+  return true;
+}
+/**
+ * @brief Checks if the pulse rate is within the acceptable range.
+ * 
+ * @param pulseRate The pulse rate value to be checked.
+ * @return True if the pulse rate is within the acceptable range, false otherwise.
+ */
+bool checkPulseRate(float pulseRate) {
+  if (pulseRate < 60 || pulseRate > 100) {
+    cout << "Pulse Rate is out of range!\n";
+    displayWarning();
+    return false;
+  }
+  return true;
+}
+/**
+ * @brief Checks if the oxygen saturation (spo2) is within the acceptable range.
+ * 
+ * @param spo2 The spo2 value to be checked.
+ * @return True if the spo2 is within the acceptable range, false otherwise.
+ */
+bool checkSpo2(float spo2) {
+  if (spo2 < 90) {
+    cout << "Oxygen Saturation out of range!\n";
+    displayWarning();
+    return false;
+  }
+  return true;
 }
--- monitor.h
+++ monitor.h
@@ -1,3 +1,7 @@
 #pragma once
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
+bool checkTemperature(float temperature);
+bool checkPulseRate(float pulseRate);
+bool checkSpo2(float spo2);
+void displayWarning();
